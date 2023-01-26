f8e1ddab8976e0a8399f05502889a2d7798a9fb6
unknown <michaelmolina679@gmail.com>
2023-01-26 15:13:46 -0600

    add Search input

--- src/components/Episodes/Episodes.jsx
+++ src/components/Episodes/Episodes.jsx
@@ -1,9 +1,33 @@
 import React from 'react';
+import { FormControl, InputLabel, Input, Button, createTheme } from '@mui/material';
+
 import EpisodesCarousel from './../EpisodesCarousel/EpisodesCarousel';
+
+const theme = createTheme({
+    components: {
+      // Name of the component
+      MuiButton: {
+        styleOverrides: {
+          // Name of the slot
+          root: {
+            // Some CSS
+            fontSize: '1rem',
+            color: '#16ACC9',
+            background: '#D2DA4B'
+          },
+        },
+      },
+    },
+  });
 
 const Episodes = () => {
     return (
         <>
+        <FormControl>
+            <InputLabel htmlFor='input' theme={theme}>Episode Name</InputLabel>
+            <Input id='input'></Input>
+            <Button type='submit'>Search</Button>
+        </FormControl>
             <h1>Top Episodios</h1>
             <EpisodesCarousel />
         </>
