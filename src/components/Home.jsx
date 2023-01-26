69d4b3b7023a5d8dec860731f9a24ecb7baad4f3
unknown <michaelmolina679@gmail.com>
2023-01-25 21:41:32 -0600

    material ui fields

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,6 @@
+import { TextField } from '@mui/material';
 import React from 'react';
+// import { searchCharacter } from '../sevices/searchService';
 import CharactersCarousel from './CharactersCarousel/CharactersCarousel';
 
 
@@ -7,6 +9,7 @@ const Home = () => {
         <>
             <h1>Top Characters</h1>
             <div className="card-grid">
+               <TextField id='input' label='Character Name' variant='outlined' color='success' focused/>
                <CharactersCarousel />
             </div>
         </>
