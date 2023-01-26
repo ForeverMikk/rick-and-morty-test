87de6528a209f7353a472f2c10c3622a3d48c3b9
unknown <michaelmolina679@gmail.com>
2023-01-25 21:31:25 -0600

    init with search bar

--- src/sevices/searchService.js
+++ src/sevices/searchService.js
@@ -1,2 +1,14 @@
 import axios from 'axios';
 
+const RICK_API = 'https://rickandmortyapi.com/api';
+
+export const searchCharacter = async(input) => {
+    try {
+        const response = await axios.get(`${RICK_API}/character?name=${input}`);
+        const data = response.data;
+
+        return data;
+    } catch (err) {
+        console.log(err);
+    }
+}
